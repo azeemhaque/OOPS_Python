5b5509e4876598a10acedf3d2a04cbd1fa229b7f
Azeem Haque <91793824+azeemhaque@users.noreply.github.com>
2023-10-08 11:00:34 +0530

    Add files via upload

---
+++ Polymorphism.py
@@ -0,0 +1,18 @@
+class Phone:
+    def __init__(self,price,brand,camera):
+        self.__price = price
+        self.brand = brand
+        self.camera = camera
+
+    def buy(self):
+        print("Buying a phone")
+
+class SmartPhone(Phone):
+    def buy(self):
+        print("Buying a smartphone")
+
+s = SmartPhone(20000,"Oneplus",13)
+
+s.buy()
+
+#Method Overriding
