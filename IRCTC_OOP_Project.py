1e6ea8df861491b9c713ebd7b259c3fe87ce6117
Azeem Haque <91793824+azeemhaque@users.noreply.github.com>
2023-10-08 10:59:29 +0530

    Add files via upload

---
+++ IRCTC_OOP_Project.py
@@ -0,0 +1,28 @@
+import requests
+class  IRCTC:
+    def __init__(self):
+
+        user_input = input("""How would you like to proceed?
+        1. Enter 1 to check live train status
+        2. Enter 2 to check PNR
+        3. Enter 3 to check train schedule""")
+
+        if user_input == "1":
+            print("live train status")
+        elif user_input == "2":
+            print("PNR")
+        else:
+            self.train_schedule()
+
+    def train_schedule(self):
+        train_no = input("Enter train no")
+        self.fetch_data(train_no)
+
+    def fetch_data(self,train_no):
+        data = requests.get("https://indianrailapi.com/api/v2/TrainSchedule/apikey/30c382602bfa67c8a7c580e6cfe2becb/TrainNumber/{}".format(train_no))
+        data = data.json()
+        print(data['Route'])
+        for i in data['Route']:
+            print(i['StationName'],"|",i['ArrivalTime'],"|",i['DepartureTime'],"|",i['Distance'],"kmsss")
+
+obj = IRCTC()
